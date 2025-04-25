e59caaca44328bb7dea5491012a27d7f745fe86d
kabinet303 <kabinet303@dc.red303.lubertex>
2025-04-25 11:53:16 +0300

    first commit

---
+++ spravka/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
+# Create your views here.
+def neglavnaya(request):
+    return render(request, 'spravka/index.html')
+
+def glavnaya(request):
+    return HttpResponse('прощайте миньенчики')
+
+def register(request):
+    return render(request, 'spravka/register.html')
+
+
+
+
+
